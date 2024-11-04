263d6c9eaf30796defc064879b2f556bde9741be
Thomas Stephan <tstephan@bess-f2r5s10.clusters.42paris.fr>
2024-11-04 18:22:17 +0100

    dup

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/04 16:33:14 by marvin            #+#    #+#             */
-/*   Updated: 2024/11/04 17:21:51 by marvin           ###   ########.fr       */
+/*   Created: 2024/11/04 17:14:14 by marvin            #+#    #+#             */
+/*   Updated: 2024/11/04 17:21:07 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include <stdlib.h>
 
 int	ft_strlen(char *str)
 {
@@ -22,11 +22,27 @@ int	ft_strlen(char *str)
 	return ((int) len);
 }
 
+char	*ft_strdup(char *src)
+{
+	char	*dup;
+	int		pos;
+
+	dup = malloc(sizeof(char) * (ft_strlen(src) + 1));
+	pos = 0;
+	while (*src)
+		dup[pos++] = *src++;
+	dup[pos] = *src;
+	return (dup);
+}
+
 // #include <stdio.h>
-// #include <stdlib.h>
+// #include <unistd.h>
 // 
 // int main(void)
 // {
-// 	printf("%d", ft_strlen("Hello World!\n"));
+// 	char hello[] = "Hello world!";
+// 
+// 	printf("%s %s\n", hello, ft_strdup(hello));
+// 
 // 	return EXIT_SUCCESS;
 // }
